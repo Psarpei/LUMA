ff71e9e3c2bf76eae97ee93a3c30f835777312e6
psarpei <pascal-fischer1206@gmx.de>
2025-06-01 20:22:12 +0200

    feat: update to new pfm methods

--- models/facerecon_model.py
+++ models/facerecon_model.py
@@ -1,11 +1,8 @@
-"""This script defines the face reconstruction model for Deep3DFaceRecon_pytorch
-"""
-
 import numpy as np
 import torch
 from .base_model import BaseModel
 from . import networks
-from .bfm import ParametricFaceModel
+from .pfm import ParametricFaceModel
 from util import util 
 from util.nvdiffrast import MeshRenderer
 from util.preprocess import estimate_norm_torch
@@ -70,13 +67,14 @@ class FaceReconModel(BaseModel):
             )
         return parser
 
-    def __init__(self, opt, fr_ckpt_path, pfm_ckpt_path):
+    def __init__(self, opt, fr_ckpt_path, pfm_ckpt_path, device):
         """Initialize this model class.
 
         Parameters:
             opt -- training/test options
             fr_ckpt_path -- checkpoint path
             pfm_ckpt_path -- parametric face model path
+            device -- device to use 'cuda' or 'cpu'
 
         A few things can be done here.
         - (required) call the initialization function of BaseModel
@@ -87,6 +85,7 @@ class FaceReconModel(BaseModel):
         self.visual_names = ['output_vis']
         self.model_names = ['net_recon']
         self.parallel_names = self.model_names + ['renderer']
+        self.device = device
 
         self.net_recon = networks.define_net_recon(
             net_recon='resnet50', use_last_fc=False, init_path=None
@@ -99,10 +98,11 @@ class FaceReconModel(BaseModel):
             ckpt_path=pfm_ckpt_path, camera_distance=10.0, focal=1015.0, center=112.0,
             is_train=False
         )
+        self.facemodel.to(self.device)
         
         fov = 2 * np.arctan(112.0 / 1015.0) * 180 / np.pi
         self.renderer = MeshRenderer(
-            rasterize_fov=fov, znear=5.0, zfar=15.0, rasterize_size=int(2 * 112), use_opengl=False #TODO: check to if to must be False
+            rasterize_fov=fov, znear=5.0, zfar=15.0, rasterize_size=int(2 * 112), use_opengl=False
         )
 
     def set_input(self, input):
@@ -117,9 +117,22 @@ class FaceReconModel(BaseModel):
         self.trans_m = input['M'].to(self.device) if 'M' in input else None
         self.image_paths = input['im_paths'] if 'im_paths' in input else None
 
+    def proj_img_to_3d(self, use_exp=True):
+        output_coeff = self.net_recon(self.input_img)
+        self.pred_face_shape, self.pred_pose, self.pred_gamma_coef, self.pred_tex_coef = \
+            self.facemodel.compute_shape_pose(output_coeff, use_exp=use_exp)
+
+        self.pred_coeffs_dict = self.facemodel.split_coeff(output_coeff) #TODO: remove later
+
+    def proj_3d_to_img(self, use_tex=True):
+        tex = self.pred_tex_coef if use_tex else None
+        self.pred_vertex, self.pred_tex, self.pred_color, self.pred_lm = self.facemodel.compute_for_render_from_shape(
+            self.pred_face_shape, self.pred_pose, self.pred_gamma_coef, tex)
+        self.pred_mask, _, self.pred_face = self.renderer(
+            self.pred_vertex, self.facemodel.face_buf, feat=self.pred_color)
+
     def forward(self):
         output_coeff = self.net_recon(self.input_img)
-        self.facemodel.to(self.device)
         self.pred_vertex, self.pred_tex, self.pred_color, self.pred_lm = \
             self.facemodel.compute_for_render(output_coeff)
         self.pred_mask, _, self.pred_face = self.renderer(
